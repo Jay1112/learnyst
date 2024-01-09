dd49e81b46a5f5d2119c2eacd0779cb143a45792
JayPatel <jay.patel1@gameskraft.com>
2024-01-09 23:52:29 +0530

    product page design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
 function Navbar(){
     return (
-        <div className="flex items-stretch justify-center py-4 app-box-shadow">
-            <p className="text-3xl md:text-2xl tracking-wide ff-monster font-semibold mx-4 text-word">Learnyst</p>
+        <nav className="bg-[var(--color-bg)] flex w-full items-stretch justify-center py-4 app-box-shadow sticky top-0 z-10">
+            <p className="flex-1 md:flex-none text-3xl md:text-2xl tracking-wide ff-monster font-semibold mx-4 text-word">Learnyst</p>
             <div className="flex-1 hidden md:flex items-stretch justify-center">
                 <ul className="w-full flex items-center justify-start px-2">
                     {
@@ -18,10 +18,10 @@ function Navbar(){
                     }
                 </ul>
             </div>
-            <div className='bg-[var(--color-fg)] text-[var(--color-bg)] rounded-md flex items-center justify-center mx-4'>
+            <div className='debug bg-[var(--color-fg)] text-[var(--color-bg)] rounded-md flex items-center justify-center mx-4'>
                 <ThemeSwitcher />
             </div>
-        </div>
+        </nav>
     );
 }
 
--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,12 +1,41 @@
 import Navbar from "../../components/Navbar/Navbar";
+import { FaArrowRight } from "react-icons/fa6";
+import TeamImage from '../../assets/team.webp';
 
-function ProductPage(){
+function ProductPage() {
+  const productAreaHeight = "h-[calc(100vh-70px)]";
 
-    return (
-        <div>
-            <Navbar/>
+  return (
+    <div className="w-screen h-screen overflow-auto relative">
+      <Navbar />
+      <div className={`${productAreaHeight} flex items-stretch justify-center flex-col-reverse md:flex-row`}>
+        <div className="flex-none  md:flex-1 flex items-center justify-stretch flex-col px-4">
+          <div className="w-full flex-1 md:w-4/5 flex items-start justify-center flex-col">
+            <h1 className="text-4xl md:text-6xl ff-poppins font-medium my-4  md:flex-none">
+              Learn with <span className="gradient-text">Learnyst</span>
+            </h1>
+            <p className="text-slate-500 text-md text-justify md:text-xl font-medium tracking-wide ff-roboto">
+              The Ultimate Platform to enhance your Software Development Skills
+              with
+              <span className="text-orange-500"> Guided Roadmaps</span> &{" "}
+              <span className="text-orange-500">Free Resources</span>.
+            </p>
+            <div className="mt-12">
+              <button
+                type="button"
+                className="flex items-center justify-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl rounded-md px-5 py-2.5 text-center text-lg font-semibold tracking-wide"
+              >
+                Get Started <FaArrowRight className="ml-3" />
+              </button>
+            </div>
+          </div>
         </div>
-    );
+        <div className="flex-none md:flex-1 flex items-center justify-center">
+            <img src={TeamImage} width={500} height={500} alt="homepage" className="w-auto md:w-3/5 h-auto"/>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
